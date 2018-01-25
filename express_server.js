948767f4caf9de50e767d75959c46b214309fe81
Laura De Witt <lauramdewitt@gmail.com>
2018-01-25 15:15:43 +0000

    added email/password forms on registration and linked them to users object

--- express_server.js
+++ express_server.js
@@ -11,6 +11,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
+
 function generateRandomString() {
     let text = "";
     const possible = "abcdefghijklmnopqrstuvwxyz0123456789";
@@ -85,7 +98,19 @@ app.post("/login", (req, res) => {
 app.post("/logout", (req, res) => {
   res.clearCookie('login');
   res.redirect("/urls");
-})
+});
+app.post("/register", (req, res) => {
+  const userID = generateRandomString();
+  let newUser = {
+    id: userID,
+    email: req.body.email,
+    password: req.body.password };
+    users[userID] = newUser;
+    console.log(newUser);
+  res.cookie("user_id", userID);
+  res.redirect("/urls");
+});
+
 
 
 app.listen(PORT, () => {
